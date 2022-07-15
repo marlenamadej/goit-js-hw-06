9c37943b15520c85c17538265730f16ef8514fd2
Marlena Madej <madejmarlena@gmail.com>
2022-07-16 00:12:31 +0200

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,21 @@
+const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+
+
+ingredients.forEach(function (ingredient, index) {
+  const list = document.querySelector("#ingredients");
+  const item = document.createElement("li");
+  console.log(item);
+  item.textContent=`${ingredient}`;
+  item.classList.add('item');
+  console.log(item.textContent);
+  list.append(item);
+});
+
+// ZROBIONE
