732254696b126c1efa4f61a8e3ec9e72f9689c07
Marlena Madej <madejmarlena@gmail.com>
2022-07-16 23:31:21 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,12 @@
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
+
+  textInput.addEventListener("input", (event) => {
     textOutput.textContent = event.currentTarget.value;
-});
+  });
 
-setOutput();
+  // ZROBIONE
 
-select.addEventListener("change", setOutput);
 
-function setOutput() {
-  const selectedOptionText = select.options[selectedOptionIndex].text;
-  textOutput.textContent = selectedOptionText;
-}
+
