118755cb90a7fea4b3c219b54eb36fdee2b390b4
Marlena Madej <madejmarlena@gmail.com>
2022-07-17 22:44:45 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,7 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
   textInput.addEventListener("input", (event) => {
-    textOutput.textContent = event.currentTarget.value;
+    if (!event.currentTarget.value) textOutput.textContent = "Anonymous";
+    else textOutput.textContent = event.currentTarget.value;
   });
   // ZROBIONE
